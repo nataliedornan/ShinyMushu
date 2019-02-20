06758af0ec61fd7f1966eae25f28b080cd2f0dc1
Laura Ingulsrud <lgi@mac.com>
2019-02-19 16:10:20 -0800

    renamed variables

--- HAB_app/app.R
+++ HAB_app/app.R
@@ -37,7 +37,7 @@ server <- function(input, output) {
    
    output$distPlot <- renderPlot({
       # generate bins based on input$bins from ui.R
-      x    <- faithful[, 2] 
+      x    <- clean_hab[, 2] 
       bins <- seq(min(x), max(x), length.out = input$bins + 1)
       
       # draw the histogram with the specified number of bins
